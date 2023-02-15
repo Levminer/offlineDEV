dc4edacf4d4e0cedbdb89409f7ddd58176a6abfd
Lőrik Levente <levminer@levminer.com>
2023-02-15 19:17:05 +0100

    Navigation improvements

--- interface/Index.tsx
+++ interface/Index.tsx
@@ -1,44 +1,14 @@
-import * as React from "react"
-import { BottomNavigation, Text, Appbar } from "react-native-paper"
-import { createDrawerNavigator } from "@react-navigation/drawer"
-import { Drawer } from "react-native-paper"
-import { View } from "react-native"
+import React from "react"
+import { BottomNavigation } from "react-native-paper"
 import Home from "./Home"
 import Browse from "./Browse"
+import Settings from "./Settings"
 
-const SideDrawer = createDrawerNavigator()
+const home = () => <Home />
+const browse = () => <Browse />
+const settings = () => <Settings />
 
-const HomeRoute = () => {
-	return (
-		<>
-			<Home />
-		</>
-	)
-}
-
-const BrowseRoute = (props) => {
-	const toggleMenu = () => {
-		props.toggleDrawer()
-	}
-
-	console.log(props)
-
-	return (
-		<>
-			<Appbar.Header elevated={true}>
-				<Appbar.Action icon="book" />
-				<Appbar.Content title="Browse" />
-				<Appbar.Action icon="menu" onPress={toggleMenu} />
-			</Appbar.Header>
-
-			<Browse />
-		</>
-	)
-}
-
-const SettingsRoute = () => <Text>Settings</Text>
-
-const Navigation = ({ navigation }) => {
+const Navigation = () => {
 	const [index, setIndex] = React.useState(0)
 	const [routes] = React.useState([
 		{ key: "home", title: "Home", focusedIcon: "home" },
@@ -46,38 +16,17 @@ const Navigation = ({ navigation }) => {
 		{ key: "settings", title: "Settings", focusedIcon: "cog" },
 	])
 
-	console.log("INDEX", navigation)
-
-	const homeRoute2 = () => <BrowseRoute {...navigation} />
-
 	const renderScene = BottomNavigation.SceneMap({
-		home: HomeRoute,
-		browse: homeRoute2,
-		settings: SettingsRoute,
+		home,
+		browse,
+		settings,
 	})
 
 	return <BottomNavigation navigationState={{ index, routes }} onIndexChange={setIndex} renderScene={renderScene} />
 }
 
-function DrawerContent() {
-	const [active, setActive] = React.useState("")
-
-	return (
-		<Drawer.Section title="Some title">
-			<Drawer.Item label="First Item" active={active === "first"} onPress={() => setActive("first")} />
-			<Drawer.Item label="Second Item" active={active === "second"} onPress={() => setActive("second")} />
-		</Drawer.Section>
-	)
-}
-
 const Index = () => {
-	return (
-		<>
-			<SideDrawer.Navigator drawerContent={() => <DrawerContent />}>
-				<SideDrawer.Screen options={{ headerShown: false }} name="a" component={Navigation} />
-			</SideDrawer.Navigator>
-		</>
-	)
+	return <Navigation />
 }
 
 export default Index
